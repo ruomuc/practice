23ab28edda28cc143b350458a753f6c4aaccb2d4
ruomu <zm252615299@gmail.com>
2020-07-26 23:01:59 +0800

    feat: object.create

--- es6/object.js
+++ es6/object.js
@@ -24,3 +24,38 @@ Object.defineProperties(book, {
   }
 })
 console.log(book)
+
+
+function shape() {
+  this.x = 0;
+  this.y = 0;
+}
+
+shape.prototype.move = function (x, y) {
+  this.x += x;
+  this.y += y;
+}
+
+const s = new shape();
+console.log(s.__proto__);
+
+var a = {};
+console.log(a);
+console.log(a.__proto__, Object.prototype);
+console.log(a.__proto__ === Object.prototype);
+
+const b = {
+  a: 1,
+  c(count) {
+    this.a = count
+  }
+}
+b.c(10);
+console.log(b);
+const bb = Object.create(b);
+console.log(bb);
+console.log(bb.__proto__);
+console.log(bb.a);
+bb.c(100);
+console.log(bb.a);
+console.log(bb.__proto__);
--- es6/tempCodeRunnerFile.js
+++ es6/tempCodeRunnerFile.js
@@ -1,3 +1,15 @@
-const arr = new Array(10)
-arr[11] = 1
-console.log(arr)
+const b = {
+  a: 1,
+  c(count) {
+    this.a = count
+  }
+}
+b.c(10);
+console.log(b);
+const bb = Object.create(b);
+console.log(bb);
+console.log(bb.__proto__);
+console.log(bb.a);
+bb.c(100);
+console.log(bb.a);
+console.log(bb.__proto__);
