9dd4746314fc62e76f3dacd531589a5931b379bc
ruomu <zm252615299@gmail.com>
2020-07-31 20:39:09 +0800

    feat: 临时提交

--- algorithm/tempCodeRunnerFile.js
+++ algorithm/tempCodeRunnerFile.js
@@ -1,2 +1,14 @@
-var a = [...'𠮷a']
-console.log(a[0], a[1])
+
+/**
+ * 层序遍历
+ * @param {*} tree 
+ * @param {*} index 
+ */
+function floorOrderTraverse(tree) {
+  for (let index = 0; index < tree.length; index++) {
+    console.log(tree[index]);
+    console.log(tree[index * 2 + 1]);
+    console.log(tree[index * 2 + 2]);
+  }
+}
+floorOrderTraverse(tree)
