a991468f26994a565fa5bae4596f2c7893e4892e
ruomu <252615299@qq.com>
2020-08-21 23:26:18 +0800

    ✨ 特性: 好数对的数目-js

---
+++ leet-code/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+calPairs = function (nums) {
+//   let tempMap = {}
+//   let count = 0
+//   for (const v of nums) {
+//     tempMap[v] ? tempMap[v]++ : (tempMap[v] = 0)
+//   }
+//   console.log(tempMap)
+//   for (const v of tempMap) {
+//     count += (v * (v - 1)) / 2
+//   }
+//   return count
+// }
