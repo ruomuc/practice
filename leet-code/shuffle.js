71ddf46ef7adccf66fbd4ee9a45338afd7355be6
ruomu <252615299@qq.com>
2020-08-31 23:40:44 +0800

    ✨ 特性: leet-code重新排列数组

---
+++ leet-code/shuffle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  let m = 0
+  let result = []
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      result.push(nums[m++])
+    } else {
+      result.push(nums[n++])
+    }
+  }
+  return result
+}
+const arr = [1, 3, 5, 2, 4, 6]
+console.log(shuffle(arr, 3))
+
