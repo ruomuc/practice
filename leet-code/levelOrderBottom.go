b6757620e8b17073c2ed2ca6fa3cd5598ce12bd4
ruomu <252615299@qq.com>
2020-09-06 23:12:34 +0800

    ✨ 特性: leet-code

---
+++ leet-code/levelOrderBottom.go
@@ -0,0 +1,32 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	var result [][]int
+	if root == nil {
+		return result
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		var tempArr []int
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			tempNode := queue[i]
+			tempArr = append(tempArr, tempNode.Val)
+			if tempNode.Left != nil {
+				queue = append(queue, tempNode.Left)
+			}
+			if tempNode.Right != nil {
+				queue = append(queue, tempNode.Right)
+			}
+		}
+		result = append([][]int{tempArr}, result...)
+		queue = queue[levelSize:]
+	}
+	return result
+}
