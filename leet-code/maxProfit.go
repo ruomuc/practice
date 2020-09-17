706db73ce97795a4a7e9dc66480108058d9c0853
ruomu <zm252615299@gmail.com>
2020-09-17 18:19:05 +0800

    ✨ 特性: leetcode

---
+++ leet-code/maxProfit.go
@@ -0,0 +1,11 @@
+package main
+
+import "math"
+
+func maxProfit(prices []int) int {
+	min := math.MaxInt32
+	maxP := 0
+	for i := range prices {
+
+	}
+}
--- leet-code/mergeTwoLists.go
+++ leet-code/mergeTwoLists.go
@@ -20,3 +20,25 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		return l2
 	}
 }
+
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head *ListNode = new(ListNode)
+	pre := head
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			pre.Next = l1
+			l1 = l1.Next
+		} else {
+			pre.Next = l2
+			l2 = l2.Next
+		}
+		pre = pre.Next
+	}
+	if l1 == nil {
+		pre.Next = l2
+	} else {
+		pre.Next = l1
+	}
+	return head.Next
+}
