b32139c8127255e5e51956eb7671782cea3314f1
ruomu <zm252615299@gmail.com>
2020-09-17 18:30:29 +0800

    🐛 修复: leetcode

--- leet-code/maxProfit.go
+++ leet-code/maxProfit.go
@@ -6,6 +6,11 @@ func maxProfit(prices []int) int {
 	min := math.MaxInt32
 	maxP := 0
 	for i := range prices {
-
+		if min > prices[i] {
+			min = prices[i]
+		} else if maxP < prices[i]-min {
+			maxP = prices[i] - min
+		}
 	}
+	return maxP
 }
