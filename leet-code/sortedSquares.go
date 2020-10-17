e18bddbb65d70702833913a607a17cac8e6f61c8
ruomu <252615299@qq.com>
2020-10-17 13:43:43 +0800

    ✨ 特性: leetcode

--- leet-code/sortedSquares.go
+++ leet-code/sortedSquares.go
@@ -1,23 +1,38 @@
 package main
 
 func sortedSquares(A []int) []int {
+	A = sortByAbs(A, 0, len(A)-1)
 
+	for i := range A {
+		A[i] = intSquare(A[i])
+	}
+	return A
+}
+
+func intSquare(a int) int {
+	return a * a
+}
+
+func intAbs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
 
 func sortByAbs(arr []int, left int, right int) []int {
-	if left < right {
+	if left > right {
 		return arr
 	}
 
 	i := left
 	j := right
 	flag := arr[left]
-
 	for i < j {
-		for i < j && arr[j] >= flag {
+		for i < j && intAbs(arr[j]) >= intAbs(flag) {
 			j--
 		}
-		for i < j && arr[j] <= flag {
+		for i < j && intAbs(arr[i]) <= intAbs(flag) {
 			i++
 		}
 
@@ -25,8 +40,8 @@ func sortByAbs(arr []int, left int, right int) []int {
 		arr[i] = arr[j]
 		arr[j] = temp
 	}
-	arr[left] = arr[j]
-	arr[j] = flag
+	arr[left] = arr[i]
+	arr[i] = flag
 	sortByAbs(arr, left, j-1)
 	sortByAbs(arr, j+1, right)
 	return arr
