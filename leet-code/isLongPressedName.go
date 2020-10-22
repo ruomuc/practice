2893638eff54eb936b688ae8fa60b8544484d2e5
ruomu <252615299@qq.com>
2020-10-22 22:12:12 +0800

    ✨ 特性: leetcode

--- leet-code/isLongPressedName.go
+++ leet-code/isLongPressedName.go
@@ -1,5 +1,16 @@
 package main
 
 func isLongPressedName(name string, typed string) bool {
-
+	var p1, p2 int
+	for p2 < len(typed) {
+		if p1 < len(name) && name[p1] == typed[p2] {
+			p1++
+			p2++
+		} else if p2 > 0 && typed[p2] == typed[p2-1] {
+			p2++
+		} else {
+			return false
+		}
+	}
+	return p1 == len(name)
 }
