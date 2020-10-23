763b804811af78f3bd0e09420c400724cd45cb8b
ruomu <zm252615299@gmail.com>
2020-10-23 20:29:31 +0800

    ✨ 特性: 临时提交

--- leet-code/isPalindromeLink.js
+++ leet-code/isPalindromeLink.js
@@ -10,5 +10,39 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-  
+  if (head == null) {
+    return head
+  }
+  let slow = head
+  let fast = head
+  while (fast != null && fast.next != null && fast.next.next != null) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  let tail = reverseLink(slow.next)
+  slow.next = null
+
+  let front = head
+  let result = true
+  while (front != null && tail != null) {
+    if (front.val !== tail.val) {
+      result = false
+    }
+    front = front.next
+    tail = tail.next
+  }
+  return result
+}
+
+function reverseLink (head) {
+  let pre = null
+  let curr = head
+  while (curr != null) {
+    let next = head.next
+    curr.next = pre
+    pre = curr
+    curr = next
+  }
+  return pre
 }
