f38d254b491b2f45fdd46d058505d190e0de1837
ruomu <252615299@qq.com>
2020-10-27 22:55:13 +0800

    ✨ 特性: 根据前序和后序遍历构造二叉树

---
+++ leet-code/constructFromPrePost.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func constructFromPrePost(pre []int, post []int) *TreeNode {
+
+}
--- leet-code/constructFromPrePost.js
+++ leet-code/constructFromPrePost.js
@@ -5,15 +5,39 @@
  *     this.left = this.right = null;
  * }
  */
-/**
+
+function TreeNode (val) {
+  this.val = val
+  this.left = this.right = null
+}
+/*
  * @param {number[]} pre
  * @param {number[]} post
  * @return {TreeNode}
  */
 var constructFromPrePost = function (pre, post) {
-  let root = new TreeNode(pre[0])
-  let dfs = function () {
-    let mid = 1
+  let len = pre.length
+  if (len === 0) {
+    return null
   }
-  dfs(pre.slice(1))
+  let root = new TreeNode(pre[0])
+  if (len === 1) {
+    return root
+  }
+
+  let L = 0
+  for (let i = 0; i < post.length; i++) {
+    if (post[i] === pre[1]) {
+      L = i + 1
+    }
+  }
+
+  root.left = constructFromPrePost(pre.slice(1, L + 1), post.slice(0, L))
+  root.right = constructFromPrePost(
+    pre.slice(L + 1, len),
+    post.slice(L, len - 1)
+  )
+  return root
 }
+
+console.log(constructFromPrePost([1, 2, 4, 5, 3, 6, 7], [4, 5, 2, 6, 7, 3, 1]))
