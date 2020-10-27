862fdb31af92b03e84490bc2647870a9fc3131d8
ruomu <252615299@qq.com>
2020-10-27 14:20:12 +0800

    ✨ 特性: 先序遍历的非递归写法 js&golang

---
+++ leet-code/preorderTraversal.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  if (root == null) {
+    return []
+  }
+  let res = []
+  let stack = []
+  stack.push(root)
+  while (stack.length > 0) {
+    let curr = stack.pop()
+    res.push(curr.val)
+    if (curr.right) {
+      stack.push(curr.right)
+    }
+    if (curr.left) {
+      stack.push(curr.left)
+    }
+  }
+  return res
+}
