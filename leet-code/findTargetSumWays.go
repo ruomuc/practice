c6436bd2f9dbdc880745f00367112253afbfc56f
ruomu <252615299@qq.com>
2020-10-29 23:17:01 +0800

    ✨ 特性: 目标和-golang

--- leet-code/findTargetSumWays.go
+++ leet-code/findTargetSumWays.go
@@ -1,5 +1,29 @@
 package main
 
+// 按题意，nums之和不会超过1000
 func findTargetSumWays(nums []int, S int) int {
+	dp := make([][]int, len(nums))
+	for i := range dp {
+		dp[i] = make([]int, 20001)
+	}
+	//初始化第一行数据
+	dp[0][nums[0]+1000]++
+	dp[0][-nums[0]+1000]++
 
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < 20001; j++ {
+			negative, positive := 0, 0
+			if j-nums[i] >= 0 {
+				negative = dp[i-1][j-nums[i]]
+			}
+			if j+nums[i] <= 2000 {
+				positive = dp[i-1][j+nums[i]]
+			}
+			dp[i][j] = negative + positive
+		}
+	}
+	if S > 1000 {
+		return 0
+	}
+	return dp[len(nums)-1][S+1000]
 }
--- leet-code/sumNumbers.js
+++ leet-code/sumNumbers.js
@@ -12,17 +12,17 @@
  */
 var sumNumbers = function (root) {
   let sum = 0
-  let dfs = function (curr, str = '') {
+  let dfs = function (curr, val) {
     if (curr == null) {
       return 0
     }
     if (curr.left == null && curr.right == null) {
-      return (sum += Number(str + curr.val))
+      return (sum += val * 10 + curr.val)
     }
-    dfs(curr.left, str + curr.val)
-    dfs(curr.right, str + curr.val)
+    dfs(curr.left, val * 10 + curr.val)
+    dfs(curr.right, val * 10 + curr.val)
   }
-  dfs(root)
+  dfs(root, 0)
   return sum
 }
 
@@ -34,6 +34,9 @@ console.log(sumNumbers({ val: 1, left: { val: 2 }, right: { val: 3 } }))
  * @return {number}
  */
 var sumNumbers2 = function (root) {
+  if (root == null) {
+    return 0
+  }
   let queueNode = []
   let queueVal = []
   let sum = 0
@@ -44,20 +47,18 @@ var sumNumbers2 = function (root) {
   while (queueNode.length != 0) {
     let tempNode = queueNode.shift()
     let tempVal = queueVal.shift()
-    console.log(tempNode, tempVal)
 
     if (tempNode.left == null && tempNode.right == null) {
       sum += tempVal
-      return
     }
 
     if (tempNode.left) {
       queueNode.push(tempNode.left)
-      queueVal.push(Number(tempVal + tempNode.left.val))
+      queueVal.push(tempVal * 10 + tempNode.left.val)
     }
     if (tempNode.right) {
       queueNode.push(tempNode.right)
-      queueVal.push(Number(tempVal + tempNode.right.val))
+      queueVal.push(tempVal * 10 + tempNode.right.val)
     }
   }
   return sum
