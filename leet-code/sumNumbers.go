793000af2d052f58007f87bad2c9c6cf3145de43
ruomu <252615299@qq.com>
2020-10-30 11:27:37 +0800

    ✨ 特性: 求根到叶子节点数字之和 深度优先加广度优先-by golang

--- leet-code/sumNumbers.go
+++ leet-code/sumNumbers.go
@@ -8,6 +8,57 @@ package main
  *     Right *TreeNode
  * }
  */
-func sumNumbers(root *TreeNode) int {
 
+// 递归，深度优先遍历
+func sumNumbers(root *TreeNode) int {
+	sum := 0
+	var dfs func(root *TreeNode, val int)
+	dfs = func(root *TreeNode, val int) {
+		if root == nil {
+			return
+		}
+		if root.Left == nil && root.Right == nil {
+			sum += val*10 + root.Val
+		}
+		dfs(root.Left, val*10+root.Val)
+		dfs(root.Right, val*10+root.Val)
+	}
+	dfs(root, 0)
+	return sum
+}
+
+// 队列，广度优先遍历
+func sumNumbers2(root *TreeNode) int {
+	sum := 0
+	if root == nil {
+		return sum
+	}
+	queueNode := make([]*TreeNode, 0)
+	queueVal := make([]int, 0)
+	// 初始化
+	queueNode = append(queueNode, root)
+	queueVal = append(queueVal, root.Val)
+
+	for len(queueNode) > 0 {
+		// 出队
+		tempNode := queueNode[0]
+		queueNode = queueNode[1:]
+		tempVal := queueVal[0]
+		queueVal = queueVal[1:]
+
+		if tempNode.Left == nil && tempNode.Right == nil {
+			sum += tempVal
+		}
+
+		if tempNode.Left != nil {
+			queueNode = append(queueNode, tempNode.Left)
+			queueVal = append(queueVal, tempVal*10+tempNode.Left.Val)
+		}
+		if tempNode.Right != nil {
+			queueNode = append(queueNode, tempNode.Right)
+			queueVal = append(queueVal, tempVal*10+tempNode.Right.Val)
+		}
+	}
+
+	return sum
 }
