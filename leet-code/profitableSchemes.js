7daa71b5a31d578cad376467e2de47a44dcdb6f8
ruomu <zm252615299@gmail.com>
2020-11-03 17:03:42 +0800

    ✨ 特性: 盈利计划,二维数据版

--- leet-code/profitableSchemes.js
+++ leet-code/profitableSchemes.js
@@ -39,4 +39,34 @@ var profitableSchemes = function (G, P, group, profit) {
   return dp[len][G][P] % mod
 }
 
+// console.log(profitableSchemes(5, 1, [2, 2], [2, 3]))
+
+// 压缩空间维度
+var profitableSchemes = function (G, P, group, profit) {
+  const len = profit.length
+  const mod = Math.pow(10, 9) + 7
+  const dp = new Array(G + 1).fill(0).map(item => {
+    return new Array(P + 1).fill(0)
+  })
+
+  // 初始化，所有目标利润为0的都一种方案，就是啥都不干。。
+  for (let j = 0; j <= G; j++) {
+    dp[j][0] = 1
+  }
+
+  for (let i = 1; i <= len; i++) {
+    for (let j = G; j >= group[i - 1]; j--) {
+      for (let k = P; k >= 0; k--) {
+        // 如果利润为负数，没有意义
+        const nk = Math.max(k - profit[i - 1], 0)
+        dp[j][k] += dp[j - group[i - 1]][nk]
+        dp[j][k] = dp[j][k] % mod
+      }
+    }
+  }
+
+  console.log('dp==', dp, len, G, P)
+  return dp[G][P] % mod
+}
+
 console.log(profitableSchemes(5, 1, [2, 2], [2, 3]))
