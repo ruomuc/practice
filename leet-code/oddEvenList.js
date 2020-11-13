fda1b521aa1acc15bd533e6d47260b7d1a248e91
ruomu <252615299@qq.com>
2020-11-13 20:00:57 +0800

    ✨ 特性: 奇偶链表

---
+++ leet-code/oddEvenList.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (head == null || head.next == null) {
+    return head
+  }
+  // 声名奇偶指针
+  let odd = head
+  let evenHead = head.next
+  let even = evenHead
+
+  while (even != null && even.next != null) {
+    odd.next = even.next
+    odd = odd.next
+    even.next = odd.next
+    even = even.next
+  }
+  odd.next = evenHead
+  return head
+}
