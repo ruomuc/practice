0c7f1d54a86684677354de359c46e2ed9cb56cff
ruomu <252615299@qq.com>
2020-11-19 18:20:46 +0800

    ✨ 特性: 临时提交

--- leet-code/coinChange.js
+++ leet-code/coinChange.js
@@ -3,4 +3,9 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function (coins, amount) {}
+var coinChange = function (coins, amount) {
+  var shark = function (coins, amount, idx) {
+    for(let k = amount/coins[idx];k<)
+    shark(coins, amount, idx)
+  }
+}
