46e51fb3b72f687e06d89aaf79dcba14c27aae81
ruomu <252615299@qq.com>
2020-12-12 12:11:53 +0800

    ✨ 特性: 摆动序列

---
+++ leet-code/wiggleMaxLength.go
@@ -0,0 +1,24 @@
+package main
+
+func wiggleMaxLength(nums []int) int {
+	n := len(nums)
+	if n < 2 {
+		return n
+	}
+	up, down := 1, 1
+	for i := 1; i < n; i++ {
+		if nums[i] > nums[i-1] {
+			up = down + 1
+		} else if nums[i] < nums[i-1] {
+			down = up + 1
+		}
+	}
+	return max(up, down)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
