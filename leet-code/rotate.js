82758a72e57eaa14203f0bc5e36feec077e932bb
ruomu <252615299@qq.com>
2020-12-19 13:01:25 +0800

    ✨ 特性: 旋转矩阵

---
+++ leet-code/rotate.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const len = matrix.length
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    for (let j = 0; j < len; j++) {
+      ;[matrix[i][j], matrix[len - i - 1][j]] = [
+        matrix[len - i - 1][j],
+        matrix[i][j]
+      ]
+    }
+  }
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      ;[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+    }
+  }
+  return matrix
+}
+
+console.log(
+  rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ])
+)
