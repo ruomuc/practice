03c53ea27324e6ac103e0d40c3cd285f5f8d4d47
ruomu <zm252615299@gmail.com>
2021-01-09 21:21:44 +0800

    ✨ 特性: 买卖股票的最佳时机 III

--- leet-code/maxProfitiii.go
+++ leet-code/maxProfitiii.go
@@ -39,3 +39,38 @@ func maxProfitii(prices []int) int {
 }
 
 // 动态规划
+func maxProfitii2(prices []int) int {
+	m := len(prices)
+	// 一共有m天
+	dp := make([][][]int, m)
+	for i := 0; i < m; i++ {
+		// 有两个状态，持有股票和未持有股票
+		dp[i] = make([][]int, 2)
+		for j := 0; j < 2; j++ {
+			// 可以交易两次
+			dp[i][j] = make([]int, 3)
+		}
+	}
+
+	// 初始化数据
+	for i := 0; i < 3; i++ {
+		dp[0][1][i] = -prices[0]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 0; j < 3; j++ {
+			if j < 2 {
+				dp[i][0][j] = max(dp[i-1][0][j], dp[i-1][1][j+1]+prices[i])
+			} else {
+				dp[i][0][j] = dp[i-1][0][j]
+			}
+			dp[i][1][j] = max(dp[i-1][1][j], dp[i-1][0][j]-prices[i])
+		}
+	}
+
+	ans := 0
+	for i := 0; i < 3; i++ {
+		ans = max(ans, dp[m-1][0][i])
+	}
+	return ans
+}
