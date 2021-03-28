b021bc74e6dd3608a4b68d06a33aadc13c3e59dd
ruomu <252615299@qq.com>
2021-03-28 17:31:42 +0800

    ✨ 特性: 二叉搜索树迭代器

--- leet-code/BSTIterator.go
+++ leet-code/BSTIterator.go
@@ -16,18 +16,27 @@ type TreeNode struct {
 }
 
 type BSTIterator struct {
+	stack []*TreeNode
+	cur   *TreeNode
 }
 
 func Constructor(root *TreeNode) BSTIterator {
-
+	return BSTIterator{cur: root}
 }
 
 func (this *BSTIterator) Next() int {
-
+	for item := this.cur; item != nil; item = item.Left {
+		this.stack = append(this.stack, item)
+	}
+	this.cur, this.stack = this.stack[len(this.stack)-1], this.stack[:len(this.stack)-1]
+	val := this.cur.Val
+	// 本题题意是要输出空节点，正常中序遍历右节点为空会继续出栈。
+	this.cur = this.cur.Right
+	return val
 }
 
 func (this *BSTIterator) HasNext() bool {
-
+	return this.cur != nil || len(this.stack) > 0
 }
 
 /**
