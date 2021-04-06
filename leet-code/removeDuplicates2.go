3dc315009bede2acd562831546abb9165ff492c7
ruomu <252615299@qq.com>
2021-04-06 22:35:23 +0800

    ✨ 特性: i am noob

--- leet-code/removeDuplicates2.go
+++ leet-code/removeDuplicates2.go
@@ -1,5 +1,17 @@
 package main
 
 func removeDuplicates(nums []int) int {
+	if len(nums) <= 2 {
+		return nums
+	}
 
+	pre, cur := 2, 2
+	for cur < len(nums) {
+		if nums[pre-2] != nums[cur] {
+			nums[pre] = nums[cur]
+			pre++
+		}
+		cur++
+	}
+	return pre
 }
