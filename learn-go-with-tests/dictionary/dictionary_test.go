913ae028038b1cd1379dfb5556202423770ff769
ruomu <252615299@qq.com>
2021-05-29 12:02:30 +0800

    ✨ 特性: learning-go-with-tests

---
+++ learn-go-with-tests/dictionary/dictionary_test.go
@@ -0,0 +1,72 @@
+package dictionary
+
+import (
+	"testing"
+)
+
+func TestUpdate(t *testing.T) {
+	word:="test"
+	description:="this is just a test"
+	dictionary := Dictionary{word:description}
+
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word, description := "test", "this is just a test"
+		_ = dictionary.Add(word, description)
+		assertDescription(t, dictionary, word, description)
+	})
+
+	t.Run("exist word", func(t *testing.T) {
+		word := "test"
+		description := "this is just a test"
+		dictionary := Dictionary{word: description}
+		err := dictionary.Add(word, description)
+
+		assertError(t, err, errorExistWord)
+	})
+}
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := errorNotFoundWord
+		assertError(t, err, want)
+	})
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s' but want '%s' given '%s'", got, want, "test")
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error '%s' want '%s' ", got, want)
+	}
+}
+
+func assertDescription(t *testing.T, dictionary Dictionary, word, description string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added work but get an error:", err)
+	}
+	if description != got {
+		t.Errorf("got '%s' but want '%s'", got, description)
+	}
+}
