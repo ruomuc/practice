8afbf228d303e2d3897c55a7d765b48972b2463c
ruomu <252615299@qq.com>
2021-05-30 11:59:15 +0800

    ✨ 特性: learn-go-with-tests/reflection

---
+++ learn-go-with-tests/reflection/reflection_test.go
@@ -0,0 +1,23 @@
+package reflection
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d but want %d ", len(got), 1)
+	}
+	if got[0] != expected {
+		t.Errorf("got '%s' but want '%s' ", got, expected)
+	}
+}
