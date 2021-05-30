f4433af20f5d1d84d70073d6bbeb4e6d5915f2fd
ruomu <252615299@qq.com>
2021-05-30 15:22:08 +0800

    🔨 重构: learn-go-with-tests/reflection/

--- learn-go-with-tests/reflection/reflection.go
+++ learn-go-with-tests/reflection/reflection.go
@@ -12,13 +12,18 @@ func walk(x interface{}, fn func(input string)) {
 		for i := 0; i < val.NumField(); i++ {
 			walk(val.Field(i).Interface(), fn)
 		}
-	case reflect.Slice:
+	case reflect.Slice, reflect.Array:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
 	case reflect.String:
 		fn(val.String())
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
 	}
+
 }
 
 func getValue(x interface{}) reflect.Value {
