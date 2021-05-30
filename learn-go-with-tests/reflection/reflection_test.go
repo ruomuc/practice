5def4ee41a9d337c1e9a6a6ae7e415d9fbeeb498
ruomu <252615299@qq.com>
2021-05-30 12:06:49 +0800

    🔨 重构: learn-go-with-tests/reflection

--- learn-go-with-tests/reflection/reflection_test.go
+++ learn-go-with-tests/reflection/reflection_test.go
@@ -1,23 +1,35 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d but want %d ", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{"Struct with one string field",
+			struct {
+				Name string
+				City string
+			}{"Chris", "beijing"},
+			[]string{"Chris", "beijing"},
+		},
 	}
-	if got[0] != expected {
-		t.Errorf("got '%s' but want '%s' ", got, expected)
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v but want %v ", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
