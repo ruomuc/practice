b064673e088fc19848ec2858014e4eee5c958e4c
ruomu <252615299@qq.com>
2021-05-30 13:48:05 +0800

    🔨 重构: learn-go-with-tests/reflection

--- learn-go-with-tests/reflection/reflection.go
+++ learn-go-with-tests/reflection/reflection.go
@@ -1,11 +1,18 @@
 package reflection
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fn(field.String())
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
 	}
 }
