b139a2c57172f94d502ece90f7bbfcd1980b26ca
ruomu <252615299@qq.com>
2021-05-30 15:03:25 +0800

    🔨 重构: 增加 slices

--- learn-go-with-tests/reflection/reflection.go
+++ learn-go-with-tests/reflection/reflection.go
@@ -5,17 +5,26 @@ import (
 )
 
 func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
-	}
+	return val
 }
