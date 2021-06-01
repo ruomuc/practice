b9962c08f7835b0a054925316f1742abca4fc04b
ruomu <zm252615299@gmail.com>
2021-06-01 22:11:01 +0800

    🐛 修复: 你能在你最喜欢的那天吃到你最喜欢的糖果吗？

--- leet-code/canEat.go
+++ leet-code/canEat.go
@@ -3,6 +3,7 @@ package main
 func canEat(candiesCount []int, queries [][]int) []bool {
 	n := len(candiesCount)
 	preSum := make([]int, n+1)
+	preSum[0] = candiesCount[0]
 	for i := 1; i < n; i++ {
 		preSum[i] = preSum[i-1] + candiesCount[i]
 	}
@@ -14,7 +15,10 @@ func canEat(candiesCount []int, queries [][]int) []bool {
 		x1 := favoriteDayi + 1
 		y1 := dailyCapi * (favoriteDayi + 1)
 
-		x2 := preSum[favoriteTypei-1] + 1
+		x2 := 1
+		if favoriteTypei > 0 {
+			x2 = preSum[favoriteDayi-1] + 1
+		}
 		y2 := preSum[favoriteTypei]
 
 		ans[i] = !(x1 > y2 || y1 < x2)
