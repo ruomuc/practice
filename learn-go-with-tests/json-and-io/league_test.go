fc4689e5b00be85cc9f042b7307466377922a964
ruomu <zm252615299@gmail.com>
2021-06-01 22:53:53 +0800

    ✨ 特性: learn-go-with-tests/json-and-io

---
+++ learn-go-with-tests/json-and-io/league_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := NewPlayerServer(&store)
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d but want %d", got, want)
+	}
+}
