ff06a54a7f21b67178e3f628b642040e1f64776e
ruomu <zm252615299@gmail.com>
2021-06-02 22:53:06 +0800

    🔨 重构: learn-go-with-tests/json-and-io

--- learn-go-with-tests/json-and-io/league_test.go
+++ learn-go-with-tests/json-and-io/league_test.go
@@ -1,22 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
 func TestLeague(t *testing.T) {
-	store := StubPlayerStore{}
-	server := NewPlayerServer(&store)
-
 	t.Run("it returns 200 on /league", func(t *testing.T) {
+		store := StubPlayerStore{}
+		server := NewPlayerServer(&store)
 		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
+		var got []Player
+
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			t.Fatalf("Unable to parse response from server '%s' into slice of Player, '%v'", response.Body, err)
+		}
+
 		assertStatus(t, response.Code, http.StatusOK)
+	})
+
+	t.Run("it return the league table as json", func(t *testing.T) {
+		wantedLeague := []Player{
+			{"Cleo", 22},
+			{"Chris", 32},
+			{"Tiest", 14},
+		}
+		store := StubPlayerStore{
+			scores:   nil,
+			winCalls: nil,
+			league:   wantedLeague,
+		}
+		server := NewPlayerServer(&store)
+
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		var got []Player
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			t.Fatalf("Unable to parse response from server '%s' into slice of Player, '%v'", response.Body, err)
+		}
+		assertStatus(t, response.Code, http.StatusOK)
+		if !reflect.DeepEqual(got, wantedLeague) {
+			t.Errorf("got %v want %v", got, wantedLeague)
+		}
 	})
 }
 
