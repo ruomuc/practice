04a527e80efb3bba01f8dd5aea94f7aaf0795c6e
ruomu <252615299@qq.com>
2021-06-03 23:12:47 +0800

    🔨 重构: learn-go-with-tests/json-and-io

---
+++ learn-go-with-tests/json-and-io/file-store.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+)
+
+type FileSystemStore struct {
+	database io.ReadSeeker
+}
+
+func (f *FileSystemStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
+
+func (f *FileSystemStore) GetPlayerScore(name string) int {
+	var wins int
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+	return wins
+}
