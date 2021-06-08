e3e856ac4af75d7f9d5b7ab99467affc5e09bc72
ruomu <zm252615299@gmail.com>
2021-06-08 22:15:13 +0800

    ✨ 特性: quiz-game增加倒计时

--- gophercises/quiz-game/main.go
+++ gophercises/quiz-game/main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFileName := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int64("tl", 30, "a time limit to answer")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -23,17 +25,30 @@ func main() {
 		exit(fmt.Sprintf("Failed to parse the provided CSV file."))
 	}
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	problems := parserLines(lines)
 	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+
+		answerChan := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChan <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nyou scored %d out of %d", correct, len(problems))
+			return
+		case answer := <-answerChan:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
-	fmt.Printf("you scored %d out of %d", correct, len(problems))
+	fmt.Printf("\nyou scored %d out of %d", correct, len(problems))
 }
 
 func parserLines(lines [][]string) []problem {
