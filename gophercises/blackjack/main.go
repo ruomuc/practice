20fdf0ed160256743499e6dd92e6d369e4aa14c2
ruomu <252615299@qq.com>
2021-06-24 23:53:57 +0800

    gophercises/blackjack

--- gophercises/blackjack/main.go
+++ gophercises/blackjack/main.go
@@ -6,32 +6,96 @@ import (
 	"strings"
 )
 
-func main() {
-	cards := deck.New(deck.Deck(3), deck.Shuffle)
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Deck = deck.New(deck.Deck(3), deck.Shuffle)
+	return ret
+}
+
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
 	var card deck.Card
-	var player, dealer Hand
 	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = draw(cards)
-			*hand = append(*hand, card)
-		}
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
 	}
+	ret.State = StatePlayerTurn
+	return ret
+}
+
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		return Stand(ret)
+	}
+	return ret
+}
+
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
+
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
+	fmt.Println("==finally hands==")
+	fmt.Println("Player:", ret.Player, "\nScore:", pScore)
+	fmt.Println("Dealer:", ret.Dealer, "\nScore:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted!")
+	case dScore > 21:
+		fmt.Println("Dealer busted!")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case pScore < dScore:
+		fmt.Println("You lose!")
+	case pScore == dScore:
+		fmt.Println("Draw!")
+	}
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
+
+func main() {
+	var gs GameState
+	gs = Shuffle(gs)
+	gs = Deal(gs)
 
 	var input string
-	for input != "s" {
-		fmt.Println("Player:", player)
-		fmt.Println("Dealer:", dealer.DealerString())
+	for gs.State == StatePlayerTurn {
+		fmt.Println("Player:", gs.Player)
+		fmt.Println("Dealer:", gs.Dealer.DealerString())
 		fmt.Println("What will you do? (s)tand or (h)it")
 		fmt.Scanf("%s\n", &input)
 		switch input {
 		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
+			gs = Hit(gs)
+		case "s":
+			gs = Stand(gs)
+		default:
+			fmt.Println("Invalid option", input)
 		}
 	}
-	fmt.Println("==finally hands==")
-	fmt.Println("Player:", player)
-	fmt.Println("Dealer:", dealer)
+	for gs.State == StateDealerTurn {
+		if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
+			gs = Hit(gs)
+		} else {
+			gs = Stand(gs)
+		}
+	}
+	gs = EndHand(gs)
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
@@ -50,4 +114,74 @@ func (h Hand) String() string {
 
 func (h Hand) DealerString() string {
 	return h[0].String() + " **hidden**"
+}
+
+func (h Hand) MinScore() int {
+	score := 0
+	for _, c := range h {
+		score += min(int(c.Rank), 10)
+	}
+	return score
+}
+
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore < 11 {
+		return minScore
+	}
+	for _, c := range h {
+		// 如果手牌小于11分
+		// 把A当做11分
+		if c.Rank == deck.Ace {
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type State int8
+
+const (
+	StatePlayerTurn State = iota
+	StateDealerTurn
+	StateHandOver
+)
+
+type GameState struct {
+	Deck   []deck.Card
+	State  State
+	Player Hand
+	Dealer Hand
+}
+
+func (gs *GameState) CurrentPlayer() *Hand {
+	switch gs.State {
+	case StatePlayerTurn:
+		return &gs.Player
+	case StateDealerTurn:
+		return &gs.Dealer
+	default:
+		panic("Wrong game state")
+	}
+}
+
+func clone(gs GameState) GameState {
+	return gs
+	ret := GameState{
+		Deck:   make([]deck.Card, len(gs.Deck)),
+		State:  gs.State,
+		Player: make(Hand, len(gs.Player)),
+		Dealer: make(Hand, len(gs.Dealer)),
+	}
+	copy(ret.Deck, gs.Deck)
+	copy(ret.Player, gs.Player)
+	copy(ret.Dealer, gs.Dealer)
+	return ret
 }
