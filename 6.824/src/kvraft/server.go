e1152b4803f5b495affe4bb787dfdb8f8381481f
ruomu <252615299@qq.com>
2022-06-10 22:58:28 +0800

    更新6.824

--- 6.824/src/kvraft/server.go
+++ 6.824/src/kvraft/server.go
@@ -1,131 +1,47 @@
 package raftkv
 
 import (
-    "labgob"
-    "labrpc"
-    "log"
-    "raft"
-    "sync"
-    "time"
+	"labgob"
+	"labrpc"
+	"log"
+	"raft"
+	"sync"
 )
 
 const Debug = 0
 
-const waitIndexIdleTime = 50 * time.Millisecond
-
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-    if Debug > 0 {
-        log.Printf(format, a...)
-    }
-    return
+	if Debug > 0 {
+		log.Printf(format, a...)
+	}
+	return
 }
 
-type OpName string
-
-const (
-    OpGet    OpName = "Get"
-    OpPut    OpName = "Put"
-    OpAppend OpName = "Append"
-)
 
 type Op struct {
-    // Your definitions here.
-    // Field names must start with capital letters,
-    // otherwise RPC will break.
-    Op    OpName
-    Key   string
-    Value string
-}
-
-type Result struct {
-    committed chan struct{}
-    value     string
+	// Your definitions here.
+	// Field names must start with capital letters,
+	// otherwise RPC will break.
 }
 
 type KVServer struct {
-    sync.Mutex
-    me      int
-    rf      *raft.Raft
-    applyCh chan raft.ApplyMsg
+	mu      sync.Mutex
+	me      int
+	rf      *raft.Raft
+	applyCh chan raft.ApplyMsg
 
-    maxraftstate int // snapshot if log grows this big
+	maxraftstate int // snapshot if log grows this big
 
-    // Your definitions here.
-    data         map[string]string
-    result       map[int]*Result
+	// Your definitions here.
 }
 
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-    // Your code here.
-    cmd := Op{
-        Op: OpGet,
-        Key: args.Key,
-    }
-    reply.WrongLeader = false
-    reply.Err = ""
-
-    DPrintf("server <%d> handle [Get], key=[%s]", kv.me, args.Key)
-    index, _, isLeader := kv.rf.Start(cmd)
-    DPrintf("server <%d> send [Get] to raft, index=[%d]", kv.me, index)
-    if !isLeader {
-        DPrintf("server <%d> handle [Get], wrong leader", kv.me)
-        reply.WrongLeader = true
-        reply.Err = "Server is not the leader"
-        return
-    }
-
-    local := make(chan struct{})
-    kv.Lock()
-    kv.result[index] = &Result{
-        committed: local,
-    }
-    kv.Unlock()
-
-    <-local
-    kv.Lock()
-    defer kv.Unlock()
-    reply.Value = kv.result[index].value
-    DPrintf("server <%d> handle [Get], cmd committed, value=[%s]", kv.me, reply.Value)
-    delete(kv.result, index)
+	// Your code here.
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
-    // Your code here.
-    cmd := Op{
-        Op: func() OpName {
-            if args.Op == "Put" {
-                return OpPut
-            }
-            return OpAppend
-        }(),
-        Key: args.Key,
-        Value: args.Value,
-    }
-    reply.WrongLeader = false
-    reply.Err = ""
-
-    DPrintf("server <%d> handle [%s], key=[%s], value=[%s]", kv.me, args.Op, args.Key, args.Value)
-    index, _, isLeader := kv.rf.Start(cmd)
-    if !isLeader {
-        DPrintf("server <%d> handle [%s] wrong leader", kv.me, args.Op)
-        reply.WrongLeader = true
-        reply.Err = "Server is not the leader"
-        return
-    }
-
-    kv.Lock()
-    local := make(chan struct{})
-    kv.result[index] = &Result{
-        committed: local,
-    }
-    kv.Unlock()
-
-    <-local
-    kv.Lock()
-    defer kv.Unlock()
-    DPrintf("server <%d> handle [%s] key=[%s] value=[%s], cmd committed", kv.me, args.Op, args.Key, args.Value)
-    delete(kv.result, index)
+	// Your code here.
 }
 
 //
@@ -135,44 +51,8 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 // turn off debug output from this instance.
 //
 func (kv *KVServer) Kill() {
-    kv.rf.Kill()
-    // Your code here, if desired.
-}
-
-func (kv *KVServer) startLocalApplyProcess() {
-    for {
-        applyMsg := <-kv.applyCh
-        index := applyMsg.CommandIndex
-        cmd := applyMsg.Command.(Op)
-        DPrintf("applying %v", applyMsg)
-        if cmd.Op == OpPut {
-            kv.Lock()
-            kv.data[cmd.Key] = cmd.Value
-            kv.Unlock()
-        }
-        if cmd.Op == OpAppend {
-            kv.Lock()
-            kv.data[cmd.Key] += cmd.Value
-            kv.Unlock()
-        }
-
-        for {
-            kv.Lock()
-            if _, ok := kv.result[index]; ok {
-                kv.Unlock()
-                break
-            }
-            kv.Unlock()
-            time.Sleep(waitIndexIdleTime)
-        }
-
-        kv.Lock()
-        if cmd.Op == OpGet {
-            kv.result[index].value = kv.data[cmd.Key]
-        }
-        kv.result[index].committed <- struct{}{}
-        kv.Unlock()
-    }
+	kv.rf.Kill()
+	// Your code here, if desired.
 }
 
 //
@@ -190,24 +70,20 @@ func (kv *KVServer) startLocalApplyProcess() {
 // for any long-running work.
 //
 func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int) *KVServer {
-    // call labgob.Register on structures you want
-    // Go's RPC library to marshall/unmarshall.
-    labgob.Register(Op{})
+	// call labgob.Register on structures you want
+	// Go's RPC library to marshall/unmarshall.
+	labgob.Register(Op{})
 
-    kv := new(KVServer)
-    kv.me = me
-    kv.maxraftstate = maxraftstate
+	kv := new(KVServer)
+	kv.me = me
+	kv.maxraftstate = maxraftstate
 
-    // You may need initialization code here.
-    kv.data = make(map[string]string)
-    kv.result = make(map[int]*Result)
+	// You may need initialization code here.
 
-    kv.applyCh = make(chan raft.ApplyMsg)
-    kv.rf = raft.Make(servers, me, persister, kv.applyCh)
+	kv.applyCh = make(chan raft.ApplyMsg)
+	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
-    // You may need initialization code here.
-    go kv.startLocalApplyProcess()
+	// You may need initialization code here.
 
-    return kv
+	return kv
 }
-
--- 6.824/src/mapreduce/schedule.go
+++ 6.824/src/mapreduce/schedule.go
@@ -1,7 +1,6 @@
 package mapreduce
 
 import "fmt"
-import "sync"
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -13,54 +12,23 @@ import "sync"
 // existing registered workers (if any) and new ones as they register.
 //
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
-    var ntasks int
-    var n_other int // number of inputs (for reduce) or outputs (for map)
-    switch phase {
-    case mapPhase:
-        ntasks = len(mapFiles)
-        n_other = nReduce
-    case reducePhase:
-        ntasks = nReduce
-        n_other = len(mapFiles)
-    }
+	var ntasks int
+	var n_other int // number of inputs (for reduce) or outputs (for map)
+	switch phase {
+	case mapPhase:
+		ntasks = len(mapFiles)
+		n_other = nReduce
+	case reducePhase:
+		ntasks = nReduce
+		n_other = len(mapFiles)
+	}
 
-    fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
-    // All ntasks tasks have to be scheduled on workers. Once all tasks
-    // have completed successfully, schedule() should return.
-    //
-    // Your code here (Part III, Part IV).
-    //
-    var done sync.WaitGroup
-    done.Add(ntasks)
-    tasks := make(chan int, ntasks)
-    for i := 0; i < ntasks; i++ {
-        tasks <- i
-    }
-    // close(tasks)
-
-    go func() {
-        for worker := range registerChan {
-            go func(worker string) {
-                for task_id := range tasks {
-                    doTaskArgs := &DoTaskArgs{
-                        JobName: jobName,
-                        File: mapFiles[task_id],
-                        Phase: phase,
-                        TaskNumber: task_id,
-                        NumOtherPhase: n_other,
-                    }
-                    if call(worker, "Worker.DoTask", doTaskArgs, nil) {
-                        done.Done()
-                    } else {
-                        tasks <- task_id
-                        return
-                    }
-                }
-            }(worker)
-        }
-    }()
-    done.Wait()
-    fmt.Printf("Schedule: %v done\n", phase)
+	// All ntasks tasks have to be scheduled on workers. Once all tasks
+	// have completed successfully, schedule() should return.
+	//
+	// Your code here (Part III, Part IV).
+	//
+	fmt.Printf("Schedule: %v done\n", phase)
 }
-
