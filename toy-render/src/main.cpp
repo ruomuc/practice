9c21659fc7885d4a103ef8f7a8bc42c4b19da212
zhangming <zm252615299@gmail.com>
2022-11-02 11:26:12 +0800

    toy-render

--- toy-render/src/main.cpp
+++ toy-render/src/main.cpp
@@ -1,12 +1,65 @@
 #include <cmath>
 #include <iostream>
 #include "tgaimage.h"
+#include "model.h"
 
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor white = TGAColor(255, 255, 255, 255);
 
+void lineDDA(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color);
 void line(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color);
+void drawDDALine();
+void drawLine();
+void drawObj();
 
 int main(int argc, char **argv)
+{
+  // drawDDALine();
+  // drawLine();
+  drawObj();
+  return 0;
+}
+
+void drawObj()
+{
+  const int width = 800;
+  const int height = 800;
+
+  // 实例化模型
+  Model *model = new Model("../obj/african_head.obj");
+
+  TGAImage image(width, height, TGAImage::RGB);
+
+  // 循环模型里的所有三角形
+  for (int i = 0; i < model->nfaces(); i++)
+  {
+    std::vector<int> face = model->face(i);
+
+    // 循环三角形三个顶点，每两个顶点连一条线
+    for (int j = 0; j < 3; j++)
+    {
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[(j + 1) % 3]);
+
+      // 因为模型空间取值范围是 [-1, 1]^3，我们要把模型坐标平移到屏幕坐标中
+      // 下面 (point + 1) * width(height) / 2 的操作学名为视口变换（Viewport Transformation）
+      int x0 = (v0.x + 1.) * width / 2.;
+      int y0 = (v0.y + 1.) * height / 2.;
+      int x1 = (v1.x + 1.) * width / 2.;
+      int y1 = (v1.y + 1.) * height / 2.;
+
+      // 画线
+      line(x0, y0, x1, y1, image, white);
+    }
+  }
+
+  image.flip_vertically();
+  image.write_tga_file("../out/line_obj.tga");
+
+  delete model;
+}
+
+void drawLine()
 {
   TGAImage image(1000, 1000, TGAImage::RGB);
   image.set(52, 41, red);
@@ -21,8 +74,25 @@ int main(int argc, char **argv)
   line(500, 1000, 0, 1000, image, red);
   line(500, 0, 0, 1000, image, red);
   line(750, 1000, 1000, 0, image, red);
-  image.write_tga_file("../out/lesson00.tga");
-  return 0;
+  image.write_tga_file("../out/Bresenhamline.tga");
+}
+
+void drawDDALine()
+{
+  TGAImage image(1000, 1000, TGAImage::RGB);
+  image.set(52, 41, red);
+  image.flip_vertically();
+  lineDDA(500, 0, 500, 0, image, red);
+  lineDDA(500, 1000, 500, 1000, image, red);
+  lineDDA(500, 1000, 500, 700, image, red);
+  lineDDA(500, 0, 500, 1000, image, red);
+  lineDDA(500, 1000, 500, 0, image, red);
+  lineDDA(500, 500, 500, 0, image, red);
+  lineDDA(500, 500, 500, 1000, image, red);
+  lineDDA(500, 1000, 0, 1000, image, red);
+  lineDDA(500, 0, 0, 1000, image, red);
+  lineDDA(750, 1000, 1000, 0, image, red);
+  image.write_tga_file("../out/ddaline.tga");
 }
 
 /**
@@ -81,7 +151,6 @@ void line(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color)
       image.set(x, y, color);
     }
     eps += dy;
-    std::cout << "x: " << x << " y: " << y << ", eps: " << eps << std::endl;
     if ((eps << 1) >= dx)
     {
       y += y_step;
@@ -90,40 +159,40 @@ void line(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color)
   }
 }
 
-// /**
-//  * DDA直线算法
-//  */
-// void line(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color)
-// {
-//   float x = x1;
-//   float y = y1;
-//   int dx = x2 - x1;
-//   int dy = y2 - y1;
+/**
+ * DDA直线算法
+ */
+void lineDDA(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color)
+{
+  float x = x1;
+  float y = y1;
+  int dx = x2 - x1;
+  int dy = y2 - y1;
 
-//   float step;
-//   float dlx, dly;
+  float step;
+  float dlx, dly;
 
-//   if (std::abs(dx) >= std::abs(dy))
-//   {
-//     step = std::abs(dx);
-//   }
-//   else
-//   {
-//     step = std::abs(dy);
-//   }
+  if (std::abs(dx) >= std::abs(dy))
+  {
+    step = std::abs(dx);
+  }
+  else
+  {
+    step = std::abs(dy);
+  }
 
-//   dlx = dx / step;
-//   dly = dy / step;
+  dlx = dx / step;
+  dly = dy / step;
 
-//   for (int i = 1; i < step; i++)
-//   {
-//     image.set(x, y, color);
-//     std::cout << "x: " << x << ", y: " << y << std::endl;
-//     x += dlx;
-//     y += dly;
-//   }
-// }
+  for (int i = 1; i < step; i++)
+  {
+    image.set(x, y, color);
+    x += dlx;
+    y += dly;
+  }
+}
 
+// 原始的画直线的方法
 // void line(int x1, int x2, int y1, int y2, TGAImage &image, TGAColor color)
 // {
 //   const float dl = 0.01;
