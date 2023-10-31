e43f741d3fa442cd97a7e7e4a9e64862f4142a89
zzzzzm <252615299@qq.com>
2023-10-31 18:16:38 +0800

    leetcode

---
+++ leet-code-150/删除有序数组中的重复项 II.go
@@ -0,0 +1,18 @@
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 2{
+		return len(nums)
+	}
+	
+	pre := 2
+	for i:= 2; i<len(nums); i++ {
+		if nums[i] != nums[pre-2] {
+			nums[pre] = nums[i]
+			pre++
+		}
+	}
+
+	return pre
+}
+
--- leet-code-150/删除有序数组中的重复项.go
+++ leet-code-150/删除有序数组中的重复项.go
@@ -1,5 +1,20 @@
 // https://leetcode.cn/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
 
 func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
 
+	fast:=0
+	slow:=0
+
+	for fast<len(nums) {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+
+	return slow+1
 }
