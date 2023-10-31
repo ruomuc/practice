e6ec2941ff18a71f3e4be9f297f85b55dd05dbb7
zzzzzm <252615299@qq.com>
2023-10-31 13:29:58 +0000

    leetcode

---
+++ leet-code-150/多数元素.go
@@ -0,0 +1,25 @@
+// https://leetcode.cn/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
+
+import "fmt"
+
+func majorityElement(nums []int) int {
+	dict := make(map[int]int)
+
+	for i,v:=range nums{
+		if _, ok := dict[i]; !ok {
+			dict[v] = 1
+		} else {
+			dict[v]++
+		}
+	}
+
+	maxKey:=0
+
+	for k,v := range dict {
+		if dict[maxKey] < v {
+			maxKey = k
+		}
+	}
+
+	return maxKey
+}
